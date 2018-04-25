9924f727ea1ab82cc58b24e4c29c1d118fd0bb7c
Bruno Lopes <37803210+brunokktro@users.noreply.github.com>
2018-04-25 19:27:29 -0300

    Delete Release-Quarantine.ps1

--- Release-Quarantine.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-Get-QuarantineMessage -RecipientAddress user@domain.com | Release-QuarantineMessage
-
-Get-QuarantineMessage -SenderAddress sender@domain.com | Release-QuarantineMessage -ReleaseToAll
-
-Get-QuarantineMessage -SenderAddress sender@domain.com | Release-QuarantineMessage -ReleaseToAll -ReportFalsePositive
